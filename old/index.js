cf5aefd1e77cafa415acb45c0d015ed9a6cd49ec
Rupayan Neogy <rneogy@mit.edu>
2019-01-18 16:05:51 -0500

    Updates

--- old/index.js
+++
@@ -1,3 +0,0 @@
-// document.getElementById('go-btn-0').addEventListener('click', function() {
-//     document.getElementById('main-col').classList.add('animated', 'fadeOutLeft');
-// });
---
+++ precache-manifest.590327647a8adb1674c84308d144a431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ba75623d0cee70765de",
+    "url": "/static/js/main.5ba75623.chunk.js"
+  },
+  {
+    "revision": "a1369086a76858aa07f1",
+    "url": "/static/js/1.a1369086.chunk.js"
+  },
+  {
+    "revision": "5ba75623d0cee70765de",
+    "url": "/static/css/main.85d6717e.chunk.css"
+  },
+  {
+    "revision": "a1369086a76858aa07f1",
+    "url": "/static/css/1.1385da10.chunk.css"
+  },
+  {
+    "revision": "833c4e795a20027875a2314bb91d874c",
+    "url": "/index.html"
+  }
+];
